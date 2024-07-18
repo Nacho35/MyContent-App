0f5e63fd74819307e687bbe6ec9602bfaf4b813e
nacho35 <ignacioevil@gmail.com>
2024-07-17 21:33:03 -0300

    App terminada 🎉️

--- src/app/services/updateSheet.js
+++ src/app/services/updateSheet.js
@@ -2,7 +2,8 @@ export async function updateGoogleSheet(
 	spreadsheetId,
 	range,
 	value,
-	accessToken
+	accessToken,
+	user
 ) {
 	const url = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${range}?valueInputOption=RAW`;
 	const options = {
@@ -26,6 +27,24 @@ export async function updateGoogleSheet(
 				`HTTP error! status: ${response.status}, message: ${errorData.error.message}`
 			);
 		}
+
+		const logResponse = await fetch("/api/logChange", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				user: user,
+				timestamp: new Date().toISOString(),
+				row: range.replace(/[^\d]/g, ""),
+				column: range.replace(/\d/g, ""),
+				status: value,
+			}),
+		});
+		if (!logResponse.ok) {
+			throw new Error(`Error logging change: ${logResponse.statusText}`);
+		}
+
 		return await response.json();
 	} catch (error) {
 		console.error("Error al actualizar Google Sheets:", error.message);
